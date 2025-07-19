a3486f24d96e4227d8b9e64dd0e93503778aabfb
Greg D <gregdingle@gmail.com>
2025-07-20 00:14:55 +0200

    node types

--- src/app/components/NodeTypes.tsx
+++ src/app/components/NodeTypes.tsx
@@ -5,30 +5,29 @@ import { Handle, Position } from "@xyflow/react";
 // NOTE: adapted from example at
 // https://codesandbox.io/p/sandbox/react-flow-node-shapes-k47gz?file=%2Fsrc%2Freact-flow-renderer%2Fstyles.css%3A17%2C1-24%2C1
 // see also https://reactflow.dev/learn/customization/custom-nodes
+// see also https://github.com/SilverDecisions/SilverDecisions/wiki/Gallery
 //
 
 const SquareNode = ({ data }: { data: AppNode["data"] }) => {
   return (
-    <div style={{ background: "#9ca8b3", padding: "14px" }}>
-      <Handle type="target" position={Position.Left} />
-      <div>{data.label}</div>
-      <Handle type="source" position={Position.Right} />
+    <div className="relative">
+      <div className="absolute -top-8 max-w-24">{data.label}</div>
+      <div className="bg-[#9ca8b3] p-4">
+        <Handle type="target" position={Position.Left} />
+        <Handle type="source" position={Position.Right} />
+      </div>
     </div>
   );
 };
 
 const CircleNode = ({ data }: { data: AppNode["data"] }) => {
   return (
-    <div
-      style={{
-        backgroundColor: "#9ca8b3",
-        padding: "14px",
-        borderRadius: "50px",
-      }}
-    >
-      <Handle type="target" position={Position.Left} />
-      <div>{data.label}</div>
-      <Handle type="source" position={Position.Right} />
+    <div className="relative">
+      <div className="absolute -top-8 max-w-24">{data.label}</div>
+      <div className="bg-[#9ca8b3] p-4 rounded-full">
+        <Handle type="target" position={Position.Left} />
+        <Handle type="source" position={Position.Right} />
+      </div>
     </div>
   );
 };
@@ -36,17 +35,15 @@ const CircleNode = ({ data }: { data: AppNode["data"] }) => {
 const TriangleNode = ({ data }: { data: AppNode["data"] }) => {
   return (
     <div className="relative">
+      <div className="absolute -top-8 max-w-24">{data.label}</div>
       <div
         style={{
-          borderTop: "50px solid transparent",
-          borderBottom: "50px solid transparent",
-          borderRight: "80px solid #9ca8b3",
+          // NOTE: this wacky CSS creates the triangle shape
+          borderTop: "20px solid transparent",
+          borderBottom: "20px solid transparent",
+          borderRight: "32px solid #9ca8b3",
         }}
-      >
-        <div className="absolute top-10 left-1/2 transform -translate-x-1/2 text-center text-sm font-medium text-white w-12">
-          {data.label}
-        </div>
-      </div>
+      ></div>
       <Handle type="source" position={Position.Left} />
     </div>
   );
