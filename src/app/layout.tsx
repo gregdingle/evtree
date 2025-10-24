0ffe349fcea287c7956d6146987b70bc7bc7a12a
Greg D <gregdingle@gmail.com>
2025-10-24 16:20:51 -0700

    Open to robots and search indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,8 +17,7 @@ export const metadata: Metadata = {
     ],
   },
   robots: {
-    // TODO: switch to true when ready for production
-    index: false,
+    index: true,
     follow: true,
   },
 };
