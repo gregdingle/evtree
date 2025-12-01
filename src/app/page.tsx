11ba7233457ac968e4ab344b5c237b04a929d128
Greg D <gregdingle@gmail.com>
2025-12-01 11:56:29 -0800

    Client valid app check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@
 import { useEffect, useState } from "react";
 
 import { ReactFlowProvider, useReactFlow } from "@xyflow/react";
-import { ReCaptchaV3Provider, initializeAppCheck } from "firebase/app-check";
+import {
+  ReCaptchaV3Provider,
+  getToken,
+  initializeAppCheck,
+} from "firebase/app-check";
 
 import { useStore } from "@/hooks/use-store";
 import { firebaseApp } from "@/lib/firebase";
@@ -21,7 +25,7 @@ export default function Home() {
   // NOTE: Prevents hydration mismatch on server-side rendering caused by
   // Zustand loading from localStorage. See
   // https://nextjs.org/docs/messages/react-hydration-error#solution-1-using-useeffect-to-run-on-the-client-only
-  const [isClient, setIsClient] = useState(false);
+  const [isValidClient, setIsValidClient] = useState(false);
 
   useEffect(() => {
     // See https://stackoverflow.com/questions/77482473/next-js-firebase-appcheck-error-recaptcha-placeholder-element-must-be-an-ele
@@ -29,25 +33,28 @@ export default function Home() {
       // @ts-expect-error: see https://firebase.google.com/docs/app-check/web/debug-provider
       self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
     }
-    initializeAppCheck(firebaseApp, {
+    const firebaseAppCheck = initializeAppCheck(firebaseApp, {
       provider: new ReCaptchaV3Provider(
         "6LepM_YrAAAAAHVY3Px_MrNBS3sp9vOKfItIU7dV",
       ),
     });
-
-    setIsClient(true);
+    getToken(firebaseAppCheck).then(() => {
+      // eslint-disable-next-line no-console
+      console.debug("[EVTree] Client validated by Firebase App Check");
+      setIsValidClient(true);
+    });
   }, []);
 
   const showHistogram = useStore(selectShowHistogram);
 
-  if (!isClient) {
+  if (!isValidClient) {
     // TODO: put some kind of loading spinner here?
     return null;
   }
 
   return (
     <ReactFlowProvider>
-      <ShareLinkLoader />
+      {isValidClient && <ShareLinkLoader />}
       <div className="evtree">
         <div className="flex h-screen flex-col">
           <div className="border-b">
