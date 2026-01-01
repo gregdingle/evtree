952553f160e45d7355cd47287a7746214aaf7ee8
Greg D <gregdingle@gmail.com>
2026-01-01 11:36:02 -0800

    Fix bug in screens

--- src/app/quick-start/page.tsx
+++ src/app/quick-start/page.tsx
@@ -41,17 +41,23 @@ export default function QuickStart() {
               width={634}
               height={385}
             />
+            <Screenshot
+              src="/quick-start/evtree-quick-start-2.png"
+              alt="screenshot"
+              width={663}
+              height={533}
+            />
           </li>
           <li className="py-2">
             Right-click on the empty canvas and <em>Add Chance Node</em>
             <Screenshot
-              src="/quick-start/evtree-quick-start-2.png"
+              src="/quick-start/evtree-quick-start-3.png"
               alt="screenshot"
               width={1198}
               height={732}
             />
             <Screenshot
-              src="/quick-start/evtree-quick-start-3.png"
+              src="/quick-start/evtree-quick-start-4.png"
               alt="screenshot"
               width={405}
               height={407}
@@ -61,7 +67,7 @@ export default function QuickStart() {
             Right-click on your new chance node and{" "}
             <em>Add Branch to... Terminal Node</em>
             <Screenshot
-              src="/quick-start/evtree-quick-start-4.png"
+              src="/quick-start/evtree-quick-start-5.png"
               alt="screenshot"
               width={585}
               height={421}
@@ -73,7 +79,7 @@ export default function QuickStart() {
             the <em>??%</em> and replace it with a probability like 0.5. Ignore
             the red warning for now.
             <Screenshot
-              src="/quick-start/evtree-quick-start-5.png"
+              src="/quick-start/evtree-quick-start-6.png"
               alt="screenshot"
               width={760}
               height={247}
@@ -83,7 +89,7 @@ export default function QuickStart() {
             Click on the <em>???</em> next to the new terminal node and replace
             it with a value like 100
             <Screenshot
-              src="/quick-start/evtree-quick-start-6.png"
+              src="/quick-start/evtree-quick-start-7.png"
               alt="screenshot"
               width={777}
               height={252}
@@ -93,7 +99,7 @@ export default function QuickStart() {
             Repeat steps 4 to 7 to add another branch to the original chance
             node. Make sure the probabilities of the branches sum to 1.0.
             <Screenshot
-              src="/quick-start/evtree-quick-start-7.png"
+              src="/quick-start/evtree-quick-start-8.png"
               alt="screenshot"
               width={730}
               height={421}
@@ -103,7 +109,7 @@ export default function QuickStart() {
             Click the <em>Calculate</em> button in the top toolbar to compute
             the expected values of your new tree.
             <Screenshot
-              src="/quick-start/evtree-quick-start-8.png"
+              src="/quick-start/evtree-quick-start-9.png"
               alt="screenshot"
               width={607}
               height={368}
